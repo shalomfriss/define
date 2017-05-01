4f60be6a8acf70a250387a472c11bc6a4810a9b9
Shalom Friss <shalom.friss@climate.com>
2017-05-01 06:39:48 -0500

    Initial Commit

---
+++ Define/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Define
+//
+//  Created by Shalom Friss on 5/1/17.
+//  Copyright © 2017 Shalom Friss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
